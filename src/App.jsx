5b57bc66d9437b8b3e118b6ee38074e366cfcf0e
Shivansh Rawat <shivanshrawat587@gmail.com>
2024-05-29 10:01:50 +0530

    use callback

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Child1 from "./components/propsdrilling/Child1";
 import Child3 from "./components/propsdrilling/Child3";
 import RefExample from "./hooksExamples/RefExample";
 import MemoExample from "./hooksExamples/MemoExample";
+import Parent from "./hooksExamples/callbackHook/Parent";
 // import { DataContext } from "./components/context/DataContext";
 
 //rule is such that ki you have to write the name of the functional component in the first letter capital letter.
@@ -50,12 +51,10 @@ import MemoExample from "./hooksExamples/MemoExample";
 function App() {
   //returning the JSX
   // const countrValue = 1000;
-//  const dataContext = useContext(DataContext);
-//  const { data, setData } = dataContext;
+  //  const dataContext = useContext(DataContext);
+  //  const { data, setData } = dataContext;
 
-// setData("1000000000000000000")
-
-
+  // setData("1000000000000000000")
 
   let [counterValue, setCountrValue] = useState(10);
 
@@ -103,8 +102,8 @@ App
       </Routes> */}
 
       {/* <RefExample /> */}
-      <MemoExample/>
-
+      {/* <MemoExample/> */}
+      <Parent/>
 
     </div>
   );
